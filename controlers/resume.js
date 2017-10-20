ae770ce081d61353869d3f283cba46471db90139
Tim3Ds <Tim3Ds@gmail.com>
2017-10-20 15:34:46 -0600

    add stuff

--- controlers/resume.js
+++ controlers/resume.js
@@ -3,14 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('Resume/index', { 
-        title: 'Resume' 
+    res.render('Resume/index', {
+        title: 'Resume'
     });
 });
 /* GET home page. */
 router.get('/portfolio', function(req, res, next) {
-    res.render('Resume/Portfolio', { 
-        title: 'Resume Portfolio' 
+    res.render('Resume/Portfolio', {
+        title: 'Resume Portfolio'
+    });
+});
+/* GET home page. */
+router.get('/letter', function(req, res, next) {
+    res.render('Resume/Letters', {
+        title: 'Resume Portfolio'
     });
 });
 
