a3689f1156e31a9054ae652b23aa0120bb966df2
Tim3Ds <Tim3Ds@gmail.com>
2017-10-22 21:11:37 -0600

    temp

--- controlers/resume.js
+++ controlers/resume.js
@@ -3,19 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('Resume/index', {
+    res.render('resume/index', {
         title: 'Resume'
     });
 });
-/* GET home page. */ 
+/* GET home page. */
 router.get('/portfolio', function(req, res, next) {
-    res.render('Resume/Portfolio', {
+    res.render('resume/Portfolio', {
+        title: 'Portfolio'
+    });
+});
+/* GET home page. */
+router.get('/Portfolio/TCMdb', function(req, res, next) {
+    res.render('resume/portfolio/TCMdb', {
+        title: 'Portfolio'
+    });
+});
+/* GET home page. */
+router.get('/portfolio/PDS', function(req, res, next) {
+    res.render('resume/portfolio/PDS', {
         title: 'Portfolio'
     });
 });
 /* GET home page. */
 router.get('/letters', function(req, res, next) {
-    res.render('Resume/Letters', {
+    res.render('resume/Letters', {
         title: 'Letters'
     });
 });
