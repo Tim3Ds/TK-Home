8d66ba27fa23f6a6d6b5cf3ed0d78167849e3e61
Tim3Ds <Tim3Ds@gmail.com>
2017-11-01 23:07:19 -0600

    fix

--- controlers/index.js
+++ controlers/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 router.get('/download/:file', function(req, res) {
     console.log('download', req.params.file);
-    var file = './public/downloads/' + req.params.file;
+    var file = '../public/downloads/' + req.params.file;
     console.log(file);
     res.download(file); // Set disposition and send it.
 });
