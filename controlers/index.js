cbeef959392cba67b9fa65cc1ef53d6fb439eb9e
Tim3Ds <Tim3Ds@gmail.com>
2017-11-01 23:05:17 -0600

    potential fix

--- controlers/index.js
+++ controlers/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Resume' });
 });
-
+router.get('/download/:file', function(req, res) {
+    console.log('download', req.params.file);
+    var file = './public/downloads/' + req.params.file;
+    console.log(file);
+    res.download(file); // Set disposition and send it.
+});
 module.exports = router;
